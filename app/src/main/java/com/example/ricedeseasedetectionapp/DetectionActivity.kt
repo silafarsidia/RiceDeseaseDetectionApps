0a0e430278c2d17199060a965306a00802e7572c
silafarsidia <silafarsidia57@gmail.com>
2022-08-16 20:21:52 +0700

    Updating Detection Activity Screen

--- app/src/main/java/com/example/ricedeseasedetectionapp/DetectionActivity.kt
+++ app/src/main/java/com/example/ricedeseasedetectionapp/DetectionActivity.kt
@@ -1,15 +1,54 @@
 package com.example.ricedeseasedetectionapp
 
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.WindowManager
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.example.ricedeseasedetectionapp.databinding.ActivityDetectionBinding
-import com.example.ricedeseasedetectionapp.databinding.ActivitySplashScreenBinding
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import java.io.File
 
 class DetectionActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetectionBinding
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,5 +65,42 @@ class DetectionActivity : AppCompatActivity() {
         }
 
         supportActionBar?.hide()
+
+        itemOnClickListener()
+    }
+
+    private fun itemOnClickListener(){
+        binding.btn1.setOnClickListener {
+            startTakePhoto()
+        }
+    }
+
+    private lateinit var currentPhotoPath: String
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val myFile = File(currentPhotoPath)
+            val result = BitmapFactory.decodeFile(myFile.path)
+
+            binding.bgPicture.setImageBitmap(result)
+        }
+    }
+
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@DetectionActivity,
+                "com.example.ricedeseasedetectionapp",
+                it
+            )
+
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
     }
 }
